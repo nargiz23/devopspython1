3d2573fe7831e36467ec045546de5f6187717a1f
nargiz23 <abylbekovnargiza22@gmail.com>
2023-04-01 14:13:18 -0400

    first commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+
+print("Hello World") #double cot is safety
+print('Hello World')
+
+#Escape Character
+#There are special characters used to insert specific charactemers in
+#a text.we use escape characters are preceded by backlash \
+# to include them in the text 
+print('He said "It\'s a lovely day to learn Python"')
+print("He said \"It\'s a lovely day to learn Python\"")
+
+#How you can print \ python?
+print("\\python?\\")
+print("Nargiza" + str(20))
