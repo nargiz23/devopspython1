bc94b267ce3ab568bccd8a4eb741c7a214ec476a
nargiz23 <abylbekovnargiza22@gmail.com>
2023-04-17 23:07:08 -0400

    practice2

--- variablepractice.py
+++ variablepractice.py
@@ -4,3 +4,10 @@ bjnmm=1
 ccgb=3
 sum=bjnmm +ccgb
 print (sum)
+def ispalindrome(word):
+    #i need to reversed version of given string
+    reversed=word [::-1]
+    return reversed.lower()==word.lower()
+
+word=input("enter a word:")
+print(ispalindrome(word))
